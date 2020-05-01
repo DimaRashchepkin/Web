28fc2463e933cfafbb669ea7ca3b5aa2c526e3d3
DimaRashchepkin <drashepkin9@gmail.com>
2020-05-01 16:48:24 +0300

    Commit 5

--- main.py
+++ main.py
@@ -20,160 +20,6 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-class RegisterForm(FlaskForm):
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль',
-                                   validators=[DataRequired()])
-    about = TextAreaField("Немного о себе")
-    submit = SubmitField('Войти')
-
-
-class LoginForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
-
-
-class NewsForm(FlaskForm):
-    title = StringField('Заголовок', validators=[DataRequired()])
-    content = TextAreaField("Содержание")
-    is_private = BooleanField("Личное")
-    submit = SubmitField('Применить')
-
-
-@app.route("/")
-def index():
-    session = db_session.create_session()
-    if current_user.is_authenticated:
-        news = session.query(News).filter(
-            (News.user == current_user) | (News.is_private != True))
-    else:
-        news = session.query(News).filter(News.is_private != True)
-    return render_template("index_old.html", news=news)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        session = db_session.create_session()
-        if session.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            email=form.email.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        session.add(user)
-        session.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    session = db_session.create_session()
-    return session.query(User).get(user_id)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        session = db_session.create_session()
-        user = session.query(User).filter(
-            User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
-@app.route('/news', methods=['GET', 'POST'])
-@login_required
-def add_news():
-    form = NewsForm()
-    if form.validate_on_submit():
-        session = db_session.create_session()
-        news = News()
-        news.title = form.title.data
-        news.content = form.content.data
-        news.is_private = form.is_private.data
-        current_user.news.append(news)
-        session.merge(current_user)
-        session.commit()
-        return redirect('/')
-    return render_template('news.html', title='Добавление новости',
-                           form=form)
-
-
-@app.route('/news/<int:id>', methods=['GET', 'POST'])
-@login_required
-def edit_news(id):
-    form = NewsForm()
-    if request.method == "GET":
-        session = db_session.create_session()
-        news = session.query(News).filter(News.id == id,
-                                          News.user == current_user).first()
-        if news:
-            form.title.data = news.title
-            form.content.data = news.content
-            form.is_private.data = news.is_private
-        else:
-            abort(404)
-    if form.validate_on_submit():
-        session = db_session.create_session()
-        news = session.query(News).filter(News.id == id,
-                                          News.user == current_user).first()
-        if news:
-            news.title = form.title.data
-            news.content = form.content.data
-            news.is_private = form.is_private.data
-            session.commit()
-            return redirect('/')
-        else:
-            abort(404)
-    return render_template('news.html', title='Редактирование новости',
-                           form=form)
-
-
-@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
-@login_required
-def news_delete(id):
-    session = db_session.create_session()
-    news = session.query(News).filter(News.id == id,
-                                      News.user == current_user).first()
-    if news:
-        session.delete(news)
-        session.commit()
-    else:
-        abort(404)
-    return redirect('/')
 
 
 def allowed_file(filename):
@@ -193,7 +39,18 @@ def resize(path):
     resized_image.save(path)
 
 
-@app.route('/new', methods=['POST', 'GET'])
+class RegisterForm(FlaskForm):
+    name = StringField('Имя:', validators=[DataRequired()])
+    surname = StringField('Фамилия:', validators=[DataRequired()])
+    email = EmailField('Логин:', validators=[DataRequired()])
+    password = PasswordField('Пароль:', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль:',
+                                   validators=[DataRequired()])
+    about = TextAreaField('Немного о себе')
+    submit = SubmitField('Зарегистрироваться')
+
+
+@app.route('/', methods=['POST', 'GET'])
 def new():
     session = db_session.create_session()
 
@@ -218,7 +75,7 @@ def new():
                 user.photo_id) + ').' + user.photo.split('.')[-1])
             user.photo_id += 1
             filename = str(user.id) + '(' + str(user.photo_id) + ').' + \
-                       file.filename.split('.')[1]
+                       file.filename.split('.')[-1]
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             user.photo = 'img/' + filename
             session.commit()
